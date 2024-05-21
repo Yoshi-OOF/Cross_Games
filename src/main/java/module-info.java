95040740478905950b448ebbcb24677e7ea387d4
Hyrox <thehyrox@gmail.com>
2024-05-21 17:39:24 +0200

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,11 @@ module org.example.main {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.net.http;
+    requires com.fasterxml.jackson.databind;
 
     exports app;
     opens app to javafx.fxml;
+
+    exports pojo;
+    opens pojo to com.fasterxml.jackson.databind;
 }
