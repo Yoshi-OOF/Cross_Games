25a84dc86661ebba69b5862cf24d5d7b9d2740ba
Hyrox <thehyrox@gmail.com>
2024-05-21 17:40:42 +0200

    update

--- src/main/java/pojo/Result.java
+++ src/main/java/pojo/Result.java
@@ -1,8 +1,15 @@
 package pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Result {
+    @JsonProperty("slug")
+    private String slug;
+    @JsonProperty("name")
     private String name;
-    //@JsonProperty("background_image")
+    @JsonProperty("background_image")
     private String backgroundImage;
+
     public String getName() { return name; }
     public String getBackgroundImage() { return backgroundImage; }
     
