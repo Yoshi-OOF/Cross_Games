17eb24a2597ac6f33544467a720ffcf5e0eb8a0b
Hyrox <thehyrox@gmail.com>
2024-05-28 17:33:19 +0200

    update

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -1,4 +1,309 @@
 package app;
 
 public class MainController {
+
+    public void searchGameTextField() {
+        // TODO implement here
+    }
+
+    public void settingsClicked() {
+        // TODO implement here
+    }
+
+    public void hoverSettingsButton() {
+        // TODO implement here
+    }
+
+    public void noHoverSettingsButton() {
+        // TODO implement here
+    }
+
+    public void addAGameClicked() {
+        // TODO implement here
+    }
+
+    public void hoverAddAGameButton() {
+        // TODO implement here
+    }
+
+    public void noHoverAddAGameButton () {
+        // TODO implement here
+    }
+
+    public void homeClicked() {
+        // TODO implement here
+    }
+
+    public void hoverHomeButton() {
+        // TODO implement here
+    }
+
+    public void noHoverHomeButton() {
+        // TODO implement here
+    }
+
+    public void doneClicked() {
+        // TODO implement here
+    }
+
+    public void hoverDoneButton() {
+        // TODO implement here
+    }
+
+    public void noHoverDoneButton() {
+        // TODO implement here
+    }
+
+    public void toDoClicked() {
+        // TODO implement here
+    }
+
+    public void hoverToDoButton() {
+        // TODO implement here
+    }
+
+    public void noHoverToDoButton() {
+        // TODO implement here
+    }
+
+    public void wishlistClicked() {
+        // TODO implement here
+    }
+
+    public void hoverWishlistButton() {
+        // TODO implement here
+    }
+
+    public void noHoverWishlistButton() {
+        // TODO implement here
+    }
+
+    public void discoverClicked() {
+        // TODO implement here
+    }
+
+    public void hoverDiscoverButton() {
+        // TODO implement here
+    }
+
+    public void noHoverDiscoverButton() {
+        // TODO implement here
+    }
+
+    public void paneGame1Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame2Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame3Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame4Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame5Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame6Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame7Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame8Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame9Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame10Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame11Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame12Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame13Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame14Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame15Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame16Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame17Clicked() {
+        // TODO implement here
+    }
+
+    public void paneGame18Clicked() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame1() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame1() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame2() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame2() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame3() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame3() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame4() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame4() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame5() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame5() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame6() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame6() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame7() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame7() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame8() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame8() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame9() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame9() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame10() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame10() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame11() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame11() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame12() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame12() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame13() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame13() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame14() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame14() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame15() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame15() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame16() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame16() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame17() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame17() {
+        // TODO implement here
+    }
+
+    public void hoverPaneGame18() {
+        // TODO implement here
+    }
+
+    public void noHoverPaneGame18() {
+        // TODO implement here
+    }
+
 }
