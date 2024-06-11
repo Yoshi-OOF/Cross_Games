c12ab62537707dbfe3b31276d6905bd7a0912a6f
Yoshi <andreolienzo@gmail.com>
2024-06-11 17:20:28 +0200

    On essaye hein

--- src/main/java/app/GameDetailController.java
+++ src/main/java/app/GameDetailController.java
@@ -19,11 +19,17 @@ import model.Game;
 
 import java.io.IOException;
 
-import static api.APIManager.imgUrl;
+public class GameDetailController {
+    private MainController mainController;
+    private Stage gameDetailDialog;
 
-import static api.APIManager.gameName;
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
 
-public class MainController {
+    public void setGameDetailDialog(Stage gameDetailDialog) {
+        this.gameDetailDialog = gameDetailDialog;
+    }
 
     private int currentIndex = 0;
 
@@ -48,7 +54,6 @@ public class MainController {
     public void initialize() {
         imageViews = new ImageView[] {imgGame1, imgGame2, imgGame3, imgGame4, imgGame5, imgGame6, imgGame7, imgGame8, imgGame9, imgGame10, imgGame11, imgGame12, imgGame13, imgGame14, imgGame15, imgGame16, imgGame17, imgGame18};
         gameTitles = new Label[] {gameTitle1, gameTitle2, gameTitle3, gameTitle4, gameTitle5, gameTitle6, gameTitle7, gameTitle8, gameTitle9, gameTitle10, gameTitle11, gameTitle12, gameTitle13, gameTitle14, gameTitle15, gameTitle16, gameTitle17, gameTitle18};
-
         panesGame = new Pane[] {paneGame1, paneGame2, paneGame3, paneGame4, paneGame5, paneGame6, paneGame7, paneGame8, paneGame9, paneGame10, paneGame11, paneGame12, paneGame13, paneGame14, paneGame15, paneGame16, paneGame17, paneGame18};
     }
 
@@ -69,18 +74,6 @@ public class MainController {
     }
 
     public void paneGameClicked(MouseEvent event){
-        Node source = (Node) event.getSource();
-        try {
-            FXMLLoader loader = new FXMLLoader(Main.class.getResource("Game_detail.fxml"));
-            Parent root = loader.load();
-            Stage currentStage = (Stage) source.getScene().getWindow();
-            Scene gamescene = new Scene(root);
-            currentStage.setScene(gamescene);
-            currentStage.setTitle(source.getId());
-            currentStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public void homeButtonClicked(MouseEvent event){
@@ -107,33 +100,6 @@ public class MainController {
     }
 
     public void searchGameButton() {
-        try {
-            Game newGame = new Game();
-            String searchText = searchGameTextFieldId.getText();
-            APIManager.setInformations(newGame, searchText);
-            Image image = new Image(APIManager.imgUrl);
-
-            if (currentIndex < imageViews.length) {
-                panesGame[currentIndex].setVisible(true);
-                ImageView currentImageView = imageViews[currentIndex];
-                currentImageView.setImage(image);
-                currentImageView.setPreserveRatio(false);
-                currentImageView.setFitHeight(200);
-                Label currentGameTitle = gameTitles[currentIndex];
-                currentGameTitle.setText(APIManager.gameName);
-                currentIndex++; // increment the index
-                if (currentIndex >= imageViews.length) {
-                    currentIndex = 0; // wrap around to the beginning
-                }
-            } else {
-                System.out.println("No more image views available");
-            }
-            System.out.println(searchText);
-        } catch (GameNotFoundException e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Game not found");
-            alert.setContentText("The game you searched for was not found by the API.");
-            alert.showAndWait();
-        }
     }
+
 }
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,7 +3,9 @@ package app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,10 +13,24 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setTitle("Cross Games");
         stage.setScene(scene);
         stage.show();
+
+        Stage gameDetailDialog = new Stage(StageStyle.DECORATED);
+        gameDetailDialog.initModality(Modality.NONE);
+        gameDetailDialog.initOwner(stage);
+
+        FXMLLoader fxmlRulesLoader = new FXMLLoader(Main.class.getResource("Game_detail.fxml"));
+        Scene sceneGameDetail = new Scene(fxmlRulesLoader.load());
+        gameDetailDialog.setScene(sceneGameDetail);
+        gameDetailDialog.setTitle("Règles");
+
+        GameDetailController gameDetailController = (GameDetailController)fxmlRulesLoader.getController();
+        gameDetailController.setMainController((MainController)fxmlLoader.getController());
+
+
     }
 
     public static void main(String[] args) {
